390b4aca0078d1545e9e188e6db43112e3ef6614
Nikolay Zhuravlev <nikzhurwork@gmail.com>
2025-06-26 20:47:55 +0300

    Add Unit tests

--- tests.py
+++ tests.py
@@ -1,24 +1,81 @@
+from conftest import *
 from main import BooksCollector
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_add_one_book(self, collector):
+        collector.add_new_book(horror_genre_book)
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 1
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_add_book_more_then_40_symbols(self, collector):
+        name = 'string more then 40 symbols string more then 40 symbols'
+        collector.add_new_book(name)
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert len(collector.get_books_genre()) == 0
+
+    def test_add_new_book_add_book_with_0_symbols(self, collector):
+        name = ''
+        collector.add_new_book(name)
+
+        assert len(collector.get_books_genre()) == 0
+
+    def test_add_new_book_add_two_similar_books(self, collector):
+        collector.add_new_book(comedy_genre_book)
+        collector.add_new_book(comedy_genre_book)
+
+        assert len(collector.get_books_genre()) == 1
+
+    def test_add_new_book_without_genre(self, collector):
+        collector.add_new_book(horror_genre_book)
+
+        assert collector.get_books_genre().get(horror_genre_book) == ''
+
+    def test_set_book_genre_set_genre(self, books_collector):
+        assert books_collector.get_book_genre(horror_genre_book) == 'Ужасы'
+
+    def test_get_books_with_specific_genre_one_book_get_list_genre(self, books_collector):
+        genre = 'Детективы'
+
+        assert books_collector.get_books_with_specific_genre(genre) == [detective_genre_book]
+
+    def test_get_books_for_children_get_list_book(self, books_collector):
+        assert books_collector.get_books_for_children() == [fantasy_genre_book, child_genre_book, comedy_genre_book]
+
+    @pytest.mark.parametrize(
+        'book, expected_result',
+        [
+            (fantasy_genre_book, True),
+            (horror_genre_book, False),
+            (detective_genre_book, True),
+            (child_genre_book, True),
+            (comedy_genre_book, True),
+        ]
+    )
+    def test_get_books_for_children_adult_books_not_included_the_list(self, collector, books_collector, book, expected_result):
+        children_books = collector.get_books_for_children()
+        assert (book in children_books) == expected_result
+
+    def test_add_book_in_favorites_add_one_book(self, collector, books_collector):
+        collector.add_book_in_favorites(fantasy_genre_book)
+        assert collector.get_list_of_favorites_books() == [fantasy_genre_book]
+
+    def test_add_book_in_favorites_similar_books(self, collector, books_collector):
+        collector.add_book_in_favorites(horror_genre_book)
+        collector.add_book_in_favorites(horror_genre_book)
+
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    def test_add_to_favorites_unlisted_books(self, collector):
+        collector.add_book_in_favorites('Книга не из списка')
+
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_delete_book_from_favorites_removes_book(self, collector, books_collector):
+        collector.add_book_in_favorites(fantasy_genre_book)
+        collector.delete_book_from_favorites(fantasy_genre_book)
+
+        assert len(collector.get_list_of_favorites_books()) == 0
